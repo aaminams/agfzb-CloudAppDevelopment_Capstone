6c573674cc66bda2ef436231e1cc831576c2d02b
aam <133568969+aaminams@users.noreply.github.com>
2023-10-13 19:53:05 +0530

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,27 +1,16 @@
-from django.db import models
-from django.utils.timezone import now
+from django.contrib import admin
+from .models import CarMake,CarModel
 
 
-# Create your models here.
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 5
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+class CarMakeAdmin(admin.ModelAdmin):
+    fields = ['name','desc','brand']
+    inlines = [CarModelInline]
+class CarModelAdmin(admin.ModelAdmin):
+    fields = ['dealer_id','name','car_type','year','color']
 
-
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
-
-
-# <HINT> Create a plain Python class `CarDealer` to hold dealer data
-
-
-# <HINT> Create a plain Python class `DealerReview` to hold review data
+admin.site.register(CarMake,CarMakeAdmin)
+admin.site.register(CarModel,CarModelAdmin)
