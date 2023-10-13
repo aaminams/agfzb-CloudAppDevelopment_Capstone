cf2535f78afa83016ea4ab4fc2da2e248dba0e85
aam <133568969+aaminams@users.noreply.github.com>
2023-10-13 19:54:00 +0530

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,16 +1,28 @@
-from django.contrib import admin
-from .models import CarMake,CarModel
+from django.db import models
+from django.utils.timezone import now
 
 
-class CarModelInline(admin.StackedInline):
-    model = CarModel
-    extra = 5
+# Create your models here.
 
-class CarMakeAdmin(admin.ModelAdmin):
-    fields = ['name','desc','brand']
-    inlines = [CarModelInline]
-class CarModelAdmin(admin.ModelAdmin):
-    fields = ['dealer_id','name','car_type','year','color']
+class CarMake(models.Model):
+    name=models.CharField(max_length=30)
+    desc=models.CharField(max_length=250)
+    brand=models.CharField(max_length=20)
+    def __str__(self):
+        print('Name is {self.name} \n Description is {self.desc} \n Brand is {self.brand}')
 
-admin.site.register(CarMake,CarMakeAdmin)
-admin.site.register(CarModel,CarModelAdmin)
+class CarModel(models.Model):
+     carmake = models.ForeignKey(CarMake, null=True, on_delete=models.CASCADE)
+     dealer_id = models.IntegerField()
+     name = models.CharField(max_length=30)
+     car_type = models.CharField(max_length=10,choices=(('SEDAN','Sedan'),('SUV','SUV'),('WAGON','Wagon')))
+     year = models.DateField(null=True)
+     color = models.CharField(max_length=10)
+     def __str__(self):
+         print('Dealer ID is {self.dealer_id} \n Name is {self.name} \n Type is {self.car_type} \n Year is {self.year} \n Color is {self.color}')
+     
+# <HINT> Create a plain Python class `CarDealer` to hold dealer data
+
+
+# <HINT> Create a plain Python class `DealerReview` to hold review data
+
